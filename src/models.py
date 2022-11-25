79348f2ab1ef8e050d238b396b5762ebb99dfb62
alexisjapas <alexis.japas.dev@pm.me>
2022-11-25 20:51:22 +0100

    adapting VDSR to FlatCNN

--- src/models.py
+++ src/models.py
@@ -1,39 +1,35 @@
 from torch import nn
 
 
-class VDSR(nn.Module):
-    def __init__(self, n_layers):
+class FlatCNN(nn.Module):
+    def __init__(self, in_size, in_channels, hidden_channels, n_layers, n_outputs):
         super().__init__()
 
         # Relu
         self.relu = nn.ReLU(inplace=True)
 
         # Input layer
-        self.input_layer = nn.Conv2d(in_channels=1,
-                                     out_channels=64,
+        self.input_layer = nn.Conv2d(in_channels=in_channels,
+                                     out_channels=hidden_channels,
                                      kernel_size=3,
                                      padding='same')
 
         # Hidden layers
         conv2d_relu = []
         for i in range(n_layers):
-            conv2d_relu.append(nn.Conv2d(in_channels=64,
-                                         out_channels=64,
+            conv2d_relu.append(nn.Conv2d(in_channels=hidden_channels,
+                                         out_channels=hidden_channels,
                                          kernel_size=3,
-                                         padding='same')
-                               )
+                                         padding='same'))
             conv2d_relu.append(nn.ReLU())
         self.conv2d_relu = nn.Sequential(*conv2d_relu)
 
         # Output layer
-        self.output_layer = nn.Conv2d(in_channels=64,
-                                      out_channels=1,
-                                      kernel_size=3,
-                                      padding='same')
+        self.output_layer = nn.Linear(in_size*in_size*hidden_channels, n_outputs)
 
     def forward(self, x):
         noisy_input = x
         x = self.input_layer(x)
         x = self.relu(x)
         x = self.conv2d_relu(x)
-        return self.output_layer(x) + noisy_input
+        return self.output_layer(x)
